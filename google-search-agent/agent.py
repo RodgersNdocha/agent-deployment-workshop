749b799949a7720f48cf690e10376f2cafa4f4b1
Rodgers Ndocha <rodgers@Esthers-MacBook.local>
2025-07-19 04:05:28 +0300

    Google agent deployment

---
+++ google-search-agent/agent.py
@@ -0,0 +1,15 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+
+
+APP_NAME="google_search_agent"
+
+root_agent = Agent(
+    name="basic_search_agent",
+    model="gemini-2.0-flash",
+    description="Agent to answer questions using Google Search.",
+    instruction="I can answer your questions by searching the internet. Just ask me anything!",
+    # google_search is a pre-built tool which allows the agent to perform Google searches.
+    tools=[google_search],
+    output_key="google_search"
+)
